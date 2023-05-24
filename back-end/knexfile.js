23006eee1b4d7743f76270f6c80f64825047db50
Hunter Perkins <112034278+WanderingKhajiit@users.noreply.github.com>
2023-05-24 15:32:22 -0400

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -3,15 +3,15 @@
  *
  * You will not need to make changes to this file.
  */
-
+/* "postgresql://postgres@localhost/postgres" */
 require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://poealumz:tXC5Dx2CyUMa4vdJs-MFa-E1iWarjQhU@drona.db.elephantsql.com/poealumz",
+  DATABASE_URL_DEVELOPMENT = "postgres://tlpporly:47_n-aSjEAkfUDSKRa3YLCgqpdc1FHUC@drona.db.elephantsql.com/tlpporly",
+  DATABASE_URL_TEST = "postgres://gcnezozo:dM0nS37SFGDArZ0ZfGEAFmt5M1UfLHdj@drona.db.elephantsql.com/gcnezozo",
+  DATABASE_URL_PREVIEW = "postgres://jonybhid:LZM7Y7RM3e0AHZztJnaOO2hCKghANZkS@drona.db.elephantsql.com/jonybhid",
   DEBUG,
 } = process.env;
 
