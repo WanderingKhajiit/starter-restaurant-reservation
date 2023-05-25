e319cb40250be8744328461721f2c554fff8b5a2
Hunter Perkins <112034278+WanderingKhajiit@users.noreply.github.com>
2023-05-25 19:40:42 -0400

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors(
 origin: "https://reservationlist-ui-ux.onrender.com/",
-credentials: true
+credentials: true,
 ));
 app.use(express.json());
 app.options("*", cors());
