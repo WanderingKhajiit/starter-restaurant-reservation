80fddda02860029bd234313023bb14531817db9f
Hunter Perkins <112034278+WanderingKhajiit@users.noreply.github.com>
2023-05-29 12:05:08 -0400

    Update ReservationForm.js

--- front-end/src/reservation/ReservationForm.js
+++ front-end/src/reservation/ReservationForm.js
@@ -41,7 +41,7 @@ export const ReservationForm = ({
             <input
               id="mobile_number"
               name="mobile_number"
-              type="number"
+              type="tel"
               required={true}
               value={reservation.mobile_number}
               maxLength="100"
